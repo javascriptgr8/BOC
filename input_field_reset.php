6ffe7f45131c86f8af1713681f54903f323440be
javascriptgr8 <88194115+javascriptgr8@users.noreply.github.com>
2022-10-10 15:25:24 +0530

    Update input_field_reset.php

--- input_field_reset.php
+++ input_field_reset.php
@@ -1,3 +1,28 @@
 var $el = $('#media_address_import');
 $el.wrap('<form>').closest('form').get(0).reset();
 $el.unwrap();
+
+  
+  
+  
+  
+  
+  
+  
+//javascript dropdown form reset  
+  <script>
+    //Reset Commodity prices form
+    $(document).on("click","#resetCommodity",function(){
+        // $("#groupFilter").val('');
+        $("#countryFilter").val('');
+        $("#com").val('');
+        $("#groupFilter").multiselect('select','0');
+        $("#groupFilter").multiselect('rebuild');
+
+        $("#countryFilter").multiselect('select','0');
+        $("#countryFilter").multiselect('rebuild');
+
+        $("#com").multiselect('select','0');
+        $("#com").multiselect('rebuild');
+    });
+</script>
